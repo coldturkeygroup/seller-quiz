a812a53b9d1a9a15911d69c927598f9afb975c6b
Aaron Huisinga <aaron@platform.marketing>
2016-12-06 01:13:11 -0600

    Bump to version 1.6.1
    1. Fix Composer vendor directory path

--- classes/class-seller-quiz.php
+++ classes/class-seller-quiz.php
@@ -5,7 +5,7 @@ namespace ColdTurkey\SellerQuiz;
 if (!defined('ABSPATH')) exit; // Exit if accessed directly.
 
 // Composer autoloader
-require_once SELLER_QUIZ_PLUGIN_PATH . '../vendor/autoload.php';
+require_once SELLER_QUIZ_PLUGIN_PATH . 'vendor/autoload.php';
 
 use ColdTurkey\SellerQuiz\FrontDesk;
 
